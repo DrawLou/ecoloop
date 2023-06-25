37b8a4f71412adae34be5c33bc95634740b3c259
Louise <zhang.louise@hotmail.fr>
2023-06-25 16:06:38 +0200

    remove img responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,54 @@ button.addEventListener('click', () => {
     });
   }
 });
+
+//Changer les images (responsive)
+window.addEventListener('DOMContentLoaded', function() {
+    const images = [
+        {
+            id: 'mockupImg1',
+            src: 'assets/img/mockupAccueil.png',
+            srcSmall: 'assets/img/mockupAccueil-petit.png'
+        },
+        {
+            id: 'mockupImg2',
+            src: 'assets/img/mockupCollecte.png',
+            srcSmall: 'assets/img/mockupCollecte-petit.png'
+        },
+        {
+            id: 'mockupImg3',
+            src: 'assets/img/mockupExplorer.png',
+            srcSmall: 'assets/img/mockupExplorer-petit.png'
+        },
+        {
+            id: 'mockupImg4',
+            src: 'assets/img/mockupSucces.png',
+            srcSmall: 'assets/img/mockupSucces-petit.png'
+        },
+        {
+            id: 'mockupImg5',
+            src: 'assets/img/mockupTelecharger.png',
+            srcSmall: 'assets/img/mockupTelecharger-petit.png'
+        }
+    ];
+
+    function changeImagesBasedOnScreenWidth() {
+        const screenWidth = window.innerWidth;
+
+        images.forEach(function(image) {
+            const imgElement = document.getElementById(image.id);
+
+            if (screenWidth <= 1080) {
+                imgElement.src = image.srcSmall;
+            } else {
+                imgElement.src = image.src;
+            }
+        });
+    }
+
+    // Écouteur d'événement pour déclencher le changement d'images lorsque la taille de l'écran change
+    window.addEventListener('resize', changeImagesBasedOnScreenWidth);
+
+    // Appel initial pour définir les images en fonction de la taille de l'écran au chargement de la page
+    changeImagesBasedOnScreenWidth();
+});
